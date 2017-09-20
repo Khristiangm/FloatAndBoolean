4b77a6a6bd20add8d6a89889ca90b0211ee32f5c
Khristian Guimaraes Marchioro (BFSI - Brazil) <KH389659@wipro.com>
2017-09-20 13:22:47 -0300

    first commit

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,36 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+   /* //WIDTH OF INT = 32 (4 BYTES)
+	int myIntValue = 5 / 3;
+
+	//WIDTH OF INT = 32 (4 BYTES)
+	float myFloatValue = 5f / 3f;
+
+    //WIDTH OF INT = 64 (8 BYTES)
+	double myDoubleValue = 5d / 3d;
+        System.out.println("My Int Value Is: " + myIntValue);
+        System.out.println("My Float Value Is: " + myFloatValue);
+        System.out.println("My Double Value Is: " + myDoubleValue);*/
+
+        //EXERCISE
+        //Convert a Given numbers of Pounds to Kilograms
+        //1. Create a Variable to store the number of Pounds
+        //2. Calculate the number of Kilograms for the number above and store in a variable
+        //3. Print out The Result
+        // NOTES :  1 Pound  is equal  to  0.45359237 kilograms..
+
+        double pounds = 200d;
+        double convertKilograms = pounds * 0.45359237d ;
+        System.out.println("Total Kilograms Is: " + convertKilograms);
+
+
+
+
+
+    }
+
+
+}
